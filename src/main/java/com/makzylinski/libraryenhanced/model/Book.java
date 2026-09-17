7268e44aabaaa15b3edffb26c2c7625649b84ee3
Maks <makzylinski@gmail.com>
2026-09-17 13:39:44 +0200

    edits

--- src/main/java/com/makzylinski/libraryenhanced/model/Book.java
+++ src/main/java/com/makzylinski/libraryenhanced/model/Book.java
@@ -7,4 +7,51 @@ public class Book {
     private int pages;
     private boolean isAvailable;
 
+    public Book(int id, String title, Author author, int pages, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
