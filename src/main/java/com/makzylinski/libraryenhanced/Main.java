a3e7ab728d7b8e8dd61f08c0de090012af74035c
Maks <makzylinski@gmail.com>
2026-09-17 11:44:21 +0200

    added option instances

--- src/main/java/com/makzylinski/libraryenhanced/Main.java
+++ src/main/java/com/makzylinski/libraryenhanced/Main.java
@@ -1,7 +1,14 @@
 package com.makzylinski.libraryenhanced;
 
+import com.makzylinski.libraryenhanced.model.Option;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Library Enhanced - starting up");
+        Option o1 = new Option(1, "1. Show all books");
+        Option o2 = new Option(1, "2. Add new book");
+        Option o3 = new Option(1, "3. Borrow a book");
+        Option o4 = new Option(1, "4. Return a book");
+        Option o5 = new Option(1, "5. Exit");
+
     }
 }
