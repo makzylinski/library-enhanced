1b4d368af17248fa77886ed759a29a5405fb4a69
Maks <makzylinski@gmail.com>
2026-09-17 11:25:27 +0200

    init

---
+++ src/main/java/com/makzylinski/libraryenhanced/Main.java
@@ -0,0 +1,7 @@
+package com.makzylinski.libraryenhanced;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Library Enhanced - starting up");
+    }
+}
