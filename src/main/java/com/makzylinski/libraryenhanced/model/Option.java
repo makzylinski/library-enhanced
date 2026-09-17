e64c21a5f5824afa6120a088341cf365de8b26ce
Maks <makzylinski@gmail.com>
2026-09-17 11:28:45 +0200

    added option

---
+++ src/main/java/com/makzylinski/libraryenhanced/model/Option.java
@@ -0,0 +1,11 @@
+package com.makzylinski.libraryenhanced.model;
+
+public class Option {
+    private int stepIndex;
+    private String label;
+
+    public Option(int stepIndex, String label) {
+        this.stepIndex = stepIndex;
+        this.label = label;
+    }
+}
