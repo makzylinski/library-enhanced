1e20e0ee03fe7ce0d346d3e8c35b67c4b3aa9418
Maks <makzylinski@gmail.com>
2026-09-17 13:34:31 +0200

    books and author class

---
+++ src/main/java/com/makzylinski/libraryenhanced/model/Author.java
@@ -0,0 +1,7 @@
+package com.makzylinski.libraryenhanced.model;
+
+public class Author {
+    private int id;
+    private String name;
+    private String lastName;
+}
---
+++ src/main/java/com/makzylinski/libraryenhanced/model/Book.java
@@ -0,0 +1,10 @@
+package com.makzylinski.libraryenhanced.model;
+
+public class Book {
+    private int id;
+    private String title;
+    private Author author;
+    private int pages;
+    private boolean isAvailable;
+
+}
